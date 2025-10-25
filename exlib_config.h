48a8e7bf27d40afb7f07dbf2b561b714554690d8
Zhijian Yan <937279457@qq.com>
2025-10-26 02:38:16 +0800

    By default, no exlib is included

--- exlib_config.h
+++ exlib_config.h
@@ -2,23 +2,23 @@
 
 // These macros are used to control which modules are enabled
 
-#define INCLUDE_EXLIB_ADC                   1
-#define INCLUDE_EXLIB_ESP_TIMER             1
-#define INCLUDE_EXLIB_GPIO                  1
-#define INCLUDE_EXLIB_GPTIMER               1
-#define INCLUDE_EXLIB_I2C                   1
-#define INCLUDE_EXLIB_I2S                   1
-#define INCLUDE_EXLIB_LEDC                  1
-#define INCLUDE_EXLIB_MCPWM                 1
-#define INCLUDE_EXLIB_PCNT                  1
-#define INCLUDE_EXLIB_RMT                   1
-#define INCLUDE_EXLIB_SD                    1
-#define INCLUDE_EXLIB_SDM                   1
-#define INCLUDE_EXLIB_SOCKET                1
-#define INCLUDE_EXLIB_SPI                   1
-#define INCLUDE_EXLIB_TEMPERATURE_SENSOR    1
-#define INCLUDE_EXLIB_TOUCH_SENSOR          1
-#define INCLUDE_EXLIB_TWAI                  1
-#define INCLUDE_EXLIB_UART                  1
-#define INCLUDE_EXLIB_USB                   1
-#define INCLUDE_EXLIB_WIFI                  1
+#define INCLUDE_EXLIB_ADC                   0
+#define INCLUDE_EXLIB_ESP_TIMER             0
+#define INCLUDE_EXLIB_GPIO                  0
+#define INCLUDE_EXLIB_GPTIMER               0
+#define INCLUDE_EXLIB_I2C                   0
+#define INCLUDE_EXLIB_I2S                   0
+#define INCLUDE_EXLIB_LEDC                  0
+#define INCLUDE_EXLIB_MCPWM                 0
+#define INCLUDE_EXLIB_PCNT                  0
+#define INCLUDE_EXLIB_RMT                   0
+#define INCLUDE_EXLIB_SD                    0
+#define INCLUDE_EXLIB_SDM                   0
+#define INCLUDE_EXLIB_SOCKET                0
+#define INCLUDE_EXLIB_SPI                   0
+#define INCLUDE_EXLIB_TEMPERATURE_SENSOR    0
+#define INCLUDE_EXLIB_TOUCH_SENSOR          0
+#define INCLUDE_EXLIB_TWAI                  0
+#define INCLUDE_EXLIB_UART                  0
+#define INCLUDE_EXLIB_USB                   0
+#define INCLUDE_EXLIB_WIFI                  0
