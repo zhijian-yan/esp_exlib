2248ff046de3302d8acf2ca27838c19ca7d7e597
Zhijian Yan <937279457@qq.com>
2026-07-04 21:05:36 +0800

    feat: includes all libraries by default

--- exlib_config.h
+++ exlib_config.h
@@ -2,23 +2,23 @@
 
 // These macros are used to control which modules are enabled
 
-#define INCLUDE_EXLIB_ADC           0
-#define INCLUDE_EXLIB_ESP_TIMER     0
-#define INCLUDE_EXLIB_GPIO          0
-#define INCLUDE_EXLIB_GPTIMER       0
-#define INCLUDE_EXLIB_I2C           0
-#define INCLUDE_EXLIB_I2S           0
-#define INCLUDE_EXLIB_LEDC          0
-#define INCLUDE_EXLIB_MCPWM         0
-#define INCLUDE_EXLIB_PCNT          0
-#define INCLUDE_EXLIB_RMT           0
-#define INCLUDE_EXLIB_SD            0
-#define INCLUDE_EXLIB_SDM           0
-#define INCLUDE_EXLIB_SOCKET        0
-#define INCLUDE_EXLIB_SPI           0
-#define INCLUDE_EXLIB_TOUCH_SENS    0
-#define INCLUDE_EXLIB_TSENS         0
-#define INCLUDE_EXLIB_TWAI          0
-#define INCLUDE_EXLIB_UART          0
-#define INCLUDE_EXLIB_USB           0
-#define INCLUDE_EXLIB_WIFI          0
+#define INCLUDE_EXLIB_ADC           1
+#define INCLUDE_EXLIB_ESP_TIMER     1
+#define INCLUDE_EXLIB_GPIO          1
+#define INCLUDE_EXLIB_GPTIMER       1
+#define INCLUDE_EXLIB_I2C           1
+#define INCLUDE_EXLIB_I2S           1
+#define INCLUDE_EXLIB_LEDC          1
+#define INCLUDE_EXLIB_MCPWM         1
+#define INCLUDE_EXLIB_PCNT          1
+#define INCLUDE_EXLIB_RMT           1
+#define INCLUDE_EXLIB_SD            1
+#define INCLUDE_EXLIB_SDM           1
+#define INCLUDE_EXLIB_SOCKET        1
+#define INCLUDE_EXLIB_SPI           1
+#define INCLUDE_EXLIB_TOUCH_SENS    1
+#define INCLUDE_EXLIB_TSENS         1
+#define INCLUDE_EXLIB_TWAI          1
+#define INCLUDE_EXLIB_UART          1
+#define INCLUDE_EXLIB_USB           1
+#define INCLUDE_EXLIB_WIFI          1
