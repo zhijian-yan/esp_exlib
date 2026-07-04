8f4c07f16cba449982a2cc7d643fd74cbb7c2dc5
Zhijian Yan <937279457@qq.com>
2026-07-04 20:58:37 +0800

    feat: add adc continuou mode

--- inc/exadc.h
+++ inc/exadc.h
@@ -9,32 +9,46 @@ extern "C" {
 #if INCLUDE_EXLIB_ADC
 
 #include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_continuous.h"
+#include "esp_adc/adc_filter.h"
+#include "esp_adc/adc_monitor.h"
 #include "esp_adc/adc_oneshot.h"
-// #include "esp_adc/adc_continuous.h"
-// #include "esp_adc/adc_monitor.h"
-// #include "esp_adc/adc_filter.h"
 
 adc_oneshot_unit_handle_t exadc_oneshot_init(adc_unit_t unit_id);
-void exadc_oneshot_deinit(adc_oneshot_unit_handle_t oneshot_unit);
-void exadc_oneshot_channel_config(adc_oneshot_unit_handle_t oneshot_unit,
+void exadc_oneshot_deinit(adc_oneshot_unit_handle_t oneshot_handle);
+void exadc_oneshot_channel_config(adc_oneshot_unit_handle_t oneshot_handle,
                                   adc_channel_t channel);
-int exadc_oneshot_read(adc_oneshot_unit_handle_t oneshot_unit,
+int exadc_oneshot_read(adc_oneshot_unit_handle_t oneshot_handle,
                        adc_channel_t channel);
 adc_cali_handle_t exadc_cali_init(adc_unit_t unit_id, adc_channel_t channel);
 void exadc_cali_deinit(adc_cali_handle_t calibration);
-int exadc_get_cali_voltage_mv(adc_oneshot_unit_handle_t oneshot_unit,
+int exadc_get_cali_voltage_mv(adc_oneshot_unit_handle_t oneshot_handle,
                               adc_channel_t channel,
                               adc_cali_handle_t calibration);
-// adc_continuous_handle_t exadc_continuous_init(adc_channel_t *channel, uint8_t
-// channel_num); void exadc_continuous_register_cbs(adc_continuous_handle_t
-// continuous_handle, adc_continuous_callback_t conv_done_cb,
-// adc_continuous_callback_t pool_ovf_cb); adc_monitor_handle_t
-// exadc_continuous_monitor_init(adc_continuous_handle_t continuous_handle,
-// adc_channel_t channel, int h_threshold, int l_threshold, adc_monitor_evt_cb_t
-// over_high_thresh, adc_monitor_evt_cb_t below_low_thresh);
-// adc_iir_filter_handle_t
-// exadc_continuous_iir_filter_init(adc_continuous_handle_t continuous_handle,
-// adc_channel_t channel, adc_digi_iir_filter_coeff_t coeff);
+adc_continuous_handle_t exadc_continuous_init(uint32_t max_store_buf_size,
+                                              uint32_t conv_frame_size);
+void exadc_continuous_deinit(adc_continuous_handle_t continuous_handle);
+void exadc_continuous_pattern_config(adc_digi_pattern_config_t *pattern,
+                                     adc_unit_t unit_id, adc_channel_t chan);
+void exadc_continuous_config(adc_continuous_handle_t continuous_handle,
+                             adc_digi_pattern_config_t *pattern,
+                             uint32_t pattern_num, uint32_t sample_freq_hz,
+                             adc_digi_convert_mode_t conv_mode);
+void exadc_continuous_register_cbs(adc_continuous_handle_t continuous_handle,
+                                   adc_continuous_callback_t conv_done_cb,
+                                   adc_continuous_callback_t pool_ovf_cb,
+                                   void *user_data);
+adc_monitor_handle_t
+exadc_continuous_monitor_init(adc_continuous_handle_t continuous_handle,
+                              adc_unit_t unit_id, adc_channel_t channel,
+                              int h_threshold, int l_threshold);
+void exadc_continuous_monitor_rigister_cbs(
+    adc_monitor_handle_t monitor_handle, adc_monitor_evt_cb_t over_high_thresh,
+    adc_monitor_evt_cb_t below_low_thresh, void *user_data);
+adc_iir_filter_handle_t
+exadc_continuous_iir_filter_init(adc_continuous_handle_t continuous_handle,
+                                 adc_unit_t unit_id, adc_channel_t channel,
+                                 adc_digi_iir_filter_coeff_t coeff);
 
 #endif
 
--- src/exadc.c
+++ src/exadc.c
@@ -3,32 +3,32 @@
 #if INCLUDE_EXLIB_ADC
 
 adc_oneshot_unit_handle_t exadc_oneshot_init(adc_unit_t unit_id) {
-    adc_oneshot_unit_handle_t oneshot_unit = NULL;
+    adc_oneshot_unit_handle_t oneshot_handle = NULL;
     adc_oneshot_unit_init_cfg_t init_cfg = {
         .unit_id = unit_id,
     };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_cfg, &oneshot_unit));
-    return oneshot_unit;
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_cfg, &oneshot_handle));
+    return oneshot_handle;
 }
 
-void exadc_oneshot_deinit(adc_oneshot_unit_handle_t oneshot_unit) {
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(oneshot_unit));
+void exadc_oneshot_deinit(adc_oneshot_unit_handle_t oneshot_handle) {
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(oneshot_handle));
 }
 
-void exadc_oneshot_channel_config(adc_oneshot_unit_handle_t oneshot_unit,
+void exadc_oneshot_channel_config(adc_oneshot_unit_handle_t oneshot_handle,
                                   adc_channel_t channel) {
     adc_oneshot_chan_cfg_t chan_cfg = {
         .atten = ADC_ATTEN_DB_12,
         .bitwidth = ADC_BITWIDTH_DEFAULT,
     };
     ESP_ERROR_CHECK(
-        adc_oneshot_config_channel(oneshot_unit, channel, &chan_cfg));
+        adc_oneshot_config_channel(oneshot_handle, channel, &chan_cfg));
 }
 
-int exadc_oneshot_read(adc_oneshot_unit_handle_t oneshot_unit,
+int exadc_oneshot_read(adc_oneshot_unit_handle_t oneshot_handle,
                        adc_channel_t channel) {
     int out_raw;
-    ESP_ERROR_CHECK(adc_oneshot_read(oneshot_unit, channel, &out_raw));
+    ESP_ERROR_CHECK(adc_oneshot_read(oneshot_handle, channel, &out_raw));
     return out_raw;
 }
 
@@ -75,98 +75,105 @@ void exadc_cali_deinit(adc_cali_handle_t calibration) {
     }
 }
 
-int exadc_get_cali_voltage_mv(adc_oneshot_unit_handle_t oneshot_unit,
+int exadc_get_cali_voltage_mv(adc_oneshot_unit_handle_t oneshot_handle,
                               adc_channel_t channel,
                               adc_cali_handle_t calibration) {
     int out_raw = 0;
     int voltage = 0;
-    ESP_ERROR_CHECK(adc_oneshot_read(oneshot_unit, channel, &out_raw));
+    ESP_ERROR_CHECK(adc_oneshot_read(oneshot_handle, channel, &out_raw));
     ESP_ERROR_CHECK(adc_cali_raw_to_voltage(calibration, out_raw, &voltage));
     return voltage;
 }
 
-// #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-// #define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
-// #define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
-// #define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
-// #else
-// #define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
-// #define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
-// #define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type2.data)
-// #endif
+adc_continuous_handle_t exadc_continuous_init(uint32_t max_store_buf_size,
+                                              uint32_t conv_frame_size) {
+    adc_continuous_handle_t continuous_handle = NULL;
+    adc_continuous_handle_cfg_t continuous_cfg = {
+        .max_store_buf_size = max_store_buf_size,
+        .conv_frame_size = conv_frame_size,
+    };
+    ESP_ERROR_CHECK(
+        adc_continuous_new_handle(&continuous_cfg, &continuous_handle));
+    return continuous_handle;
+}
 
-// adc_continuous_handle_t exadc_continuous_init(adc_channel_t *channel, uint8_t
-// channel_num)
-// {
-//     adc_continuous_handle_t continuous_handle = NULL;
-//     adc_continuous_handle_cfg_t adc_config = {
-//         .max_store_buf_size = 1024,
-//         .conv_frame_size = 256,
-//     };
-//     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config,
-//     &continuous_handle)); adc_continuous_config_t dig_cfg = {
-//         .sample_freq_hz = SOC_ADC_SAMPLE_FREQ_THRES_HIGH,
-//         .conv_mode = ADC_CONV_SINGLE_UNIT_1,
-//         .format = EXAMPLE_ADC_OUTPUT_TYPE,
-//     };
-//     adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
-//     dig_cfg.pattern_num = channel_num;
-//     for (int i = 0; i < channel_num; i++) {
-//         adc_pattern[i].atten = ADC_ATTEN_DB_12;
-//         adc_pattern[i].channel = channel[i] & 0x7;
-//         adc_pattern[i].unit = ADC_UNIT_1;
-//         adc_pattern[i].bit_width = ADC_BITWIDTH_DEFAULT;
-//     }
-//     dig_cfg.adc_pattern = adc_pattern;
-//     ESP_ERROR_CHECK(adc_continuous_config(continuous_handle, &dig_cfg));
-//     return continuous_handle;
-// }
+void exadc_continuous_deinit(adc_continuous_handle_t continuous_handle) {
+    ESP_ERROR_CHECK(adc_continuous_deinit(continuous_handle));
+}
 
-// void exadc_continuous_register_cbs(adc_continuous_handle_t continuous_handle,
-// adc_continuous_callback_t conv_done_cb, adc_continuous_callback_t
-// pool_ovf_cb)
-// {
-//     adc_continuous_evt_cbs_t cbs = {
-//         .on_conv_done = conv_done_cb,
-//         .on_pool_ovf = pool_ovf_cb,
-//     };
-//     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(continuous_handle,
-//     &cbs, NULL));
-// }
+void exadc_continuous_pattern_config(adc_digi_pattern_config_t *pattern,
+                                     adc_unit_t unit_id, adc_channel_t chan) {
+    pattern->atten = ADC_ATTEN_DB_12;
+    pattern->bit_width = ADC_BITWIDTH_DEFAULT;
+    pattern->channel = chan & 0x7;
+    pattern->unit = unit_id;
+}
 
-// adc_monitor_handle_t exadc_continuous_monitor_init(adc_continuous_handle_t
-// continuous_handle, adc_channel_t channel, int h_threshold, int l_threshold,
-// adc_monitor_evt_cb_t over_high_thresh, adc_monitor_evt_cb_t below_low_thresh)
-// {
-//     adc_monitor_handle_t monitor_handle = NULL;
-//     adc_monitor_config_t zero_crossing_config = {
-//         .adc_unit = ADC_UNIT_1,
-//         .channel = channel,
-//         .h_threshold = h_threshold,
-//         .l_threshold = l_threshold,
-//     };
-//     ESP_ERROR_CHECK(adc_new_continuous_monitor(continuous_handle,
-//     &zero_crossing_config, &monitor_handle)); adc_monitor_evt_cbs_t
-//     zero_crossing_cbs = {
-//         .on_over_high_thresh = over_high_thresh,
-//         .on_below_low_thresh = below_low_thresh,
-//     };
-//     ESP_ERROR_CHECK(adc_continuous_monitor_register_event_callbacks(monitor_handle,
-//     &zero_crossing_cbs, NULL)); return monitor_handle;
-// }
+void exadc_continuous_config(adc_continuous_handle_t continuous_handle,
+                             adc_digi_pattern_config_t *pattern,
+                             uint32_t pattern_num, uint32_t sample_freq_hz,
+                             adc_digi_convert_mode_t conv_mode) {
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = sample_freq_hz,
+        .conv_mode = conv_mode,
+    };
+    dig_cfg.pattern_num = pattern_num;
+    dig_cfg.adc_pattern = pattern;
+    ESP_ERROR_CHECK(adc_continuous_config(continuous_handle, &dig_cfg));
+}
 
-// adc_iir_filter_handle_t
-// exadc_continuous_iir_filter_init(adc_continuous_handle_t continuous_handle,
-// adc_channel_t channel, adc_digi_iir_filter_coeff_t coeff)
-// {
-//     adc_iir_filter_handle_t iir_filter_handle = NULL;
-//     adc_continuous_iir_filter_config_t iir_filter_cfg = {
-//         .channel = channel,
-//         .coeff = coeff,
-//         .unit = ADC_UNIT_1
-//     };
-//     ESP_ERROR_CHECK(adc_new_continuous_iir_filter(continuous_handle,
-//     &iir_filter_cfg, &iir_filter_handle)); return iir_filter_handle;
-// }
+void exadc_continuous_register_cbs(adc_continuous_handle_t continuous_handle,
+                                   adc_continuous_callback_t conv_done_cb,
+                                   adc_continuous_callback_t pool_ovf_cb,
+                                   void *user_data) {
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = conv_done_cb,
+        .on_pool_ovf = pool_ovf_cb,
+    };
+    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(continuous_handle,
+                                                            &cbs, user_data));
+}
+
+adc_monitor_handle_t
+exadc_continuous_monitor_init(adc_continuous_handle_t continuous_handle,
+                              adc_unit_t unit_id, adc_channel_t channel,
+                              int h_threshold, int l_threshold) {
+    adc_monitor_handle_t monitor_handle = NULL;
+    adc_monitor_config_t zero_crossing_cfg = {
+        .adc_unit = unit_id,
+        .channel = channel,
+        .h_threshold = h_threshold,
+        .l_threshold = l_threshold,
+    };
+    ESP_ERROR_CHECK(adc_new_continuous_monitor(
+        continuous_handle, &zero_crossing_cfg, &monitor_handle));
+    return monitor_handle;
+}
+
+void exadc_continuous_monitor_rigister_cbs(
+    adc_monitor_handle_t monitor_handle, adc_monitor_evt_cb_t over_high_thresh,
+    adc_monitor_evt_cb_t below_low_thresh, void *user_data) {
+    adc_monitor_evt_cbs_t zero_crossing_cbs = {
+        .on_over_high_thresh = over_high_thresh,
+        .on_below_low_thresh = below_low_thresh,
+    };
+    ESP_ERROR_CHECK(adc_continuous_monitor_register_event_callbacks(
+        monitor_handle, &zero_crossing_cbs, user_data));
+}
+
+adc_iir_filter_handle_t
+exadc_continuous_iir_filter_init(adc_continuous_handle_t continuous_handle,
+                                 adc_unit_t unit_id, adc_channel_t channel,
+                                 adc_digi_iir_filter_coeff_t coeff) {
+    adc_iir_filter_handle_t iir_filter_handle = NULL;
+    adc_continuous_iir_filter_config_t iir_filter_cfg = {
+        .channel = channel,
+        .coeff = coeff,
+        .unit = unit_id,
+    };
+    ESP_ERROR_CHECK(adc_new_continuous_iir_filter(
+        continuous_handle, &iir_filter_cfg, &iir_filter_handle));
+    return iir_filter_handle;
+}
 
 #endif
